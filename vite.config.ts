a3f84879c9cb56d9f0e2e884571fb4a8e32f4ac7
Ilian Petrov <ilianppetrov@gmail.com>
2026-01-17 21:29:18 +0200

    update

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => ({
+export default defineConfig(() => ({
   base: "/tichu-score/",
   plugins: [
     devtools(),
